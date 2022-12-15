0f8f0daaf87a7228cd3d523fe297d1f9401ce3eb
MahfuzNazib <nazibmahfuz60@gmail.com>
2022-12-15 14:56:54 +0600

    One Onre Problem Solved

--- StringProblems/ShuffleString/ShuffleString/Program.cs
+++ StringProblems/ShuffleString/ShuffleString/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string s = "codeleet";
+            int[] indices = { 4, 5, 6, 7, 0, 2, 1, 3 };
+            RestoreString(s, indices);
         }
+        public static void RestoreString(string s, int[] indices)
+        {
+            string output = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (indices[i] == i)
+                {
+                    Console.WriteLine("Value : " + indices[i] + " and Index : " + i);
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+        }
+
     }
 }
